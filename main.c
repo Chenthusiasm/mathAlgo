cf8eb9ab2993a17cbf014c6150fb93b71b14c1bc
Chenthusiasm <vachen@gmail.com>
2023-04-30 09:18:23 -0500

    Added initial main.c file.

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    printf("testing...\n");
+
+    return 0;
+}
